c3acd5d652cdeebc6364e99b48f236541a073473
Nadine Rodriguez <inadinerod@gmail.com>
2022-06-01 20:29:38 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var footer = document.querySelector("footer");
+// console.log(footer);
+footer.style.backgroundColor = "#d7ddf3";
+
+var tagline = document.querySelector(".tagline");
+//console.log(tagline);
+tagline.innerText = 'Roses really smell like poopoo';
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.5em";
+address.style.fontSize = "1.75em";
+
+var everyday = document.querySelector("footer h3");
+everyday.innerHTML = "We're here for you <em>every day</em> of the week!";
